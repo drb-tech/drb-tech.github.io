05ef2de55999a57561f89f6562cf2b9d732ce7e0
rezk <ahmedrezkofficial@gmail.com>
2024-09-19 14:30:15 +0300

    add email

---
+++ newsletter.php
@@ -0,0 +1,11 @@
+<?php
+
+  $to      = 'drb.technology.team@gmail.com';
+    $subject = "New Subscription: " . $_POST['email'];
+    $message = 'hello';
+    $headers = 'From: webmaster@example.com'       . "\r\n" .
+                 'Reply-To: webmaster@example.com' . "\r\n" .
+                 'X-Mailer: PHP/' . phpversion();
+
+    mail($to, $subject, $message, $headers);
+?>
